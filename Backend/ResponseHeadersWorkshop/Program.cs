7e17320927ed92a7a04e39a3b989c86bcecd17b1
kayschluter <kay.schluter@hotmail.com>
2025-05-26 13:11:15 +0200

    .

--- Backend/ResponseHeadersWorkshop/Program.cs
+++ Backend/ResponseHeadersWorkshop/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var MyPolicy = "_myAllowSpecificOrigins";
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyPolicy,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
+
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -12,6 +24,16 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("strict-transport-security", "max-age=31536000");
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
+    await next();
+});
+
+app.UseCors(MyPolicy);
 
 app.UseHttpsRedirection();
 
--- Frontend/client/vite.config.js
+++ Frontend/client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; frame-ancestors 'self'; form-action 'self';",
+      "X-Content-Type-Options": "nosniff",
+      "X-Frame-Options": "DENY",
+    },
+  },
+});
